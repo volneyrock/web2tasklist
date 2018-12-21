2f75885f0e9add1664c448964474f21235eabdbd
Volney Casas <volneyrock@gmail.com>
2018-12-21 13:43:41 -0200

    definição dos models e tabela tarefas

--- models/02_tabelas.py
+++ models/02_tabelas.py
@@ -2,5 +2,6 @@ Tarefas = db.define_tables('tarefas',
                            Field('titulo', 'string', label='Título'),
                            Field('status', 'boolean', label='Tarefa concluída',
                                  default=False),
+                           Field('obs', 'text', label='OBS.'),
                            auth.signature,
                            format='%(titulo)s')
